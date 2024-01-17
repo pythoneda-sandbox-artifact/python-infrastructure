f65ecee3c86a7e33a52f562bbeb45e90d99ee660
rydnr <github@acm-sl.org>
2024-01-17 19:21:44 +0100

    Added vim and emacs settings

--- pythoneda/sandbox/artifact/infrastructure/local_sandbox.py
+++ pythoneda/sandbox/artifact/infrastructure/local_sandbox.py
@@ -138,3 +138,11 @@ class LocalSandbox(Sandbox):
         :rtype: pythoneda.shared.artifact.events.TagPushed
         """
         return await cls.instance().push_tag_after_CommittedChangesTagged(event)
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
